9808b7c5c545650ddf37f7e86e92f826aa571019
Niko Rintamaki <rr.niko@icoud.com>
2021-03-04 18:26:24 +0200

    Update to use render_templates

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Worlds!'
+    return render_template('index.html')
