abd4d10f4208564cb6984c570827131f24fda418
Niko Rintamaki <rr.niko@icoud.com>
2021-03-05 16:35:31 +0200

    Update Flask to recognize faces

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from face_r import recognize
 
 app = Flask(__name__)
 
@@ -6,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/face')
+def face():
+    name = recognize(1).capitalize()
+    return render_template('index.html', name=name)
