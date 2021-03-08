3677f236985660a29f30c2e08e53dc7be159817b
Niko Rintamaki <rr.niko@icoud.com>
2021-03-08 12:11:38 +0200

    Rewrite to reduce duplication

--- db_query.py
+++ db_query.py
@@ -1,88 +1,98 @@
 import psycopg2
-import sys
+from psycopg2 import OperationalError, DatabaseError
 
-# Using PostgreSQL 13.2 with database test1.pgsql
-
-# Improve: serious duplication must be removed
-
-# Ask recipe for drink if available. Else return 'Drink not available'.
-def recipe(drink):
-    recipe = None
-    con = None
-
-    # If connection works
+def create_connection(db_name, db_user, db_password, db_host):
+    connection = None
     try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
 
-        con = psycopg2.connect(host='localhost', database='test1', user='mikko',
-            password='baari')
-
-        cur = con.cursor()
-
-        # Join neccesary tables and ask for drink recipe if possible to make
-        cur.execute("SELECT quantity, pumps.id FROM drinks JOIN recipes ON drinks.id = recipes.drink_id JOIN ingredients ON recipes.ingredient_id = ingredients.id LEFT JOIN pumps ON pumps.ingredient_id = ingredients.id WHERE drink=%s AND pumps.ingredient_id IS NOT NULL", (drink,))
-
-        # If there is recipe print it
-        if cur.rowcount > 0:
-            # for record in cur:
-            #     print(record)
-            recipe = cur.fetchall()
-            print(recipe)
-        else:
-            print('Drink not available.')
-
-    # Else report error and exit
-    except psycopg2.DatabaseError as e:
-
-        print(f'Error {e}')
-        sys.exit(1)
-
-    # after close connection
-    finally:
-
-        if con:
-            con.close()
-    return recipe
-
-def get_drinks():
-    drinks = None
-    con = None
-
-    # If connection works
+def execute_query(connection, query):
+    cursor = connection.cursor()
     try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
-        con = psycopg2.connect(host='localhost', database='test1', user='mikko',
-            password='baari')
+def execute_read_query(connection, query, vars):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query, (vars,))
+        result = cursor.fetchall()
+        return result
+    except DatabaseError as e:
+        print(f"The error '{e}' occurred")
 
-        cur = con.cursor()
+def get_all_drinks():
+    # Connect
+    connection = create_connection(
+        "test1", "mikko", "baari", "127.0.0.1"
+    )
 
-        # Join neccesary tables and ask for drink recipe if possible to make
-        cur.execute("SELECT drink FROM drinks")
+    query = """
+    SELECT drink FROM drinks
+    """
+    vars = None
 
-        # If there is recipe print it
-        if cur.rowcount > 0:
-            # for record in cur:
-            #     print(record)
-            drinks = cur.fetchall()
-            print(drinks)
-        else:
-            print('No drinks')
+    result = execute_read_query(connection, query, vars)
+    return result
 
-    # Else report error and exit
-    except psycopg2.DatabaseError as e:
+def get_available_drinks():
+    # Connect
+    connection = create_connection(
+        "test1", "mikko", "baari", "127.0.0.1"
+    )
 
-        print(f'Error {e}')
-        sys.exit(1)
+    query = """
+    SELECT DISTINCT drink FROM drinks
+    JOIN recipes ON drinks.id = recipes.drink_id
+    JOIN ingredients ON recipes.ingredient_id = ingredients.id
+    JOIN pumps ON ingredients.id = pumps.ingredient_id
+    WHERE pumps.ingredient_id IS NOT NULL
+    """
+    vars = None
 
-    # after close connection
-    finally:
+    result = execute_read_query(connection, query, vars)
+    return result
 
-        if con:
-            con.close()
-    return drinks
+# Gets drink name
+# Rerturn recipe if available else None
+def get_availble_recipe(drink):
+    # Connect
+    connection = create_connection(
+        "test1", "mikko", "baari", "127.0.0.1"
+    )
 
+    query = """
+    SELECT quantity, pumps.id FROM drinks
+    JOIN recipes ON drinks.id = recipes.drink_id
+    JOIN ingredients ON recipes.ingredient_id = ingredients.id
+    LEFT JOIN pumps ON pumps.ingredient_id = ingredients.id
+    WHERE drink=%s AND pumps.ingredient_id IS NOT NULL
+    """
+    vars = drink
+
+    result = execute_read_query(connection, query, vars)
+    if len(result) > 0:
+        return result
+    else:
+        return None
+
+def Main():
+    print(get_all_drinks())
+    print(get_available_drinks())
+    print(get_availble_recipe('Kelkka'))
 
 if __name__ == '__main__':
-    # drink to make
-    get_drinks()
-    drink = 'Rommicola'
-    recipe(drink)
+    Main()
