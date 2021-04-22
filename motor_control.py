1dc5e1215700865cc8dc0c1f7959634969af57a8
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2021-04-22 18:59:05 +0300

    Make pump control into a class

--- motor_control.py
+++ motor_control.py
@@ -2,114 +2,59 @@ import time
 from time import sleep
 import RPi.GPIO as GPIO
 
-## Mockup stuff
-recipes = [
-    {
-        "name": "vodka_battery",
-        "ingredients": [
-            ("vodka", 40),
-            ("red_bull", 300)
-        ]        
-    },
-    {
-        "name": "jumalan_ruoska",
-        "ingredients": [
-            ("absintti", 40),
-            ("jaloviina", 40)
-        ]
-    }
-]
+class PumpConf:
+    def __init__(self):
+        self.SPEED_RATIO_SLOW = 3000/40  # s/ml
+        self.SPEED_RATIO_FAST = 3000/100  # s/ml
+        self.LIMIT_SLOW = 100 # ml
+        self.SLOW_DC = 40 # %
+        self.FAST_DC = 100 # %
 
-pump_configuration = {
-    "vodka": 1,
-    "jaloviina": 2,
-    "absintti": 3,
-    "red_bull": 4
-}
+        self.hz = 100
+        self.pumpCount = 9
+        self.pumps = {}
 
+        # Allocate necessary output pins
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        for i in range(1, self.pumpCount+1):
+            GPIO.setup(i, GPIO.OUT)
+            self.pumps[1] = GPIO.PWM(i, self.hz)
 
-## Constants
-SPEED_RATIO_SLOW = 3000/40  # s/ml
-SPEED_RATIO_FAST = 3000/100  # s/ml
-LIMIT_SLOW = 100 # ml
-SLOW_DC = 40 # %
-FAST_DC = 100 # %
+    ## Map ingrediend amounts to pump times (two different pump speeds for over and under 100ml)
+    def ml_to_pump_time(self, ml):
+        ratio = self.SPEED_RATIO_SLOW if ml < self.LIMIT_SLOW else self.SPEED_RATIO_FAST
+        return ml * ratio
 
+    ## Pump control function
+    def make_drink(self, ingredients):
+        """
+        Controls the pumps to produce the drink defined by `ingredients`.
+        
+        :param ingredients: dictionary of ml per pump {pumpNo [int]: ml [int]}
+        :return:
+        """
+        # Timings
+        max_time = max(map(lambda ingredient: self.ml_to_pump_time(ingredients[ingredient]), ingredients))
+        start_time = time.time()
+        time = 0
 
-## Helpers
-# Map ingrediend amounts to pump times (two different pump speeds for over and under 100ml)
-def ml_to_pump_time(ml):
-    ratio = SPEED_RATIO_SLOW if ml < LIMIT_SLOW else SPEED_RATIO_FAST
-    return ml * ratio
+        # Start all pumps with ingredients included in the recipe
+        for ingredient in ingredients:
+            dc = self.SLOW_DC if ingredients[ingredient] < self.LIMIT_SLOW else self.FAST_DC
+            try:
+                self.pumps[ingredient].start(dc)
+            except:
+                print("Ingredient required for recipe not configured!")
+                raise
 
+        # Run until last ingredient done
+        while time < max_time:
+            # Check if pump has run long enough
+            for ingredient in ingredients:
+                # Stop pumps that have run long enough
+                if self.ml_to_pump_time(ingredients[ingredient]) < time:
+                    self.pumps[ingredients[ingredient]].stop()
 
-
-## Initial setup
-# Allocate necessary output pins
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-# for i in range(1, 18+1):
-#     GPIO.setup(i, GPIO.OUT)
-
-# Create pump PWM instances
-# pumps = {}
-# for ing in pump_configuration:
-#     pumps[ing] = GPIO.PWM(pump_configuration[ing], 50)  # FIXME Does 50Hz work?
-
-
-def test_pump():
-    dc = 0
-    hz = 100
-    # pump = pumps["vodka"]
-    GPIO.setup(21, GPIO.OUT)
-    pump = GPIO.PWM(21, hz)
-    try:
-        print('Starting pump')
-        pump.start(dc)
-    except:
-        print(f"Lol pump {pump} didn't start")
-        raise
-
-    # sleep(5)
-
-    for i in range(100, 50, -1):
-        print(f'Setting dc to {i}')
-        pump.ChangeDutyCycle(i)
-        sleep(0.2)
-
-    try:
-        print('Stopping pump')
-        pump.stop()
-    except:
-        print(f"Lol pump {pump} didn't stop")
-        raise
-
-
-## Pump control function
-# TODO Probably needs to be made async
-def make_drink(recipe):
-    # Timings
-    max_time = max(map(lambda ing: ml_to_pump_time(ing[1]), recipe.ingredients))
-    start_time = time.time()
-    time = 0
-
-    # Start all pumps with ingredients included in the recipe
-    for ing in recipe.ingredients:
-        dc = SLOW_DC if ing[1] < LIMIT_SLOW else FAST_DC
-        try:
-            pumps[ing].start(dc)
-        except:
-            print("Ingredient required for recipe not configured!")
-            raise
-
-    # Run until last ingredient done
-    while time < max_time:
-        # Check if pump has run long enough
-        for ing in recipe.ingredients:
-            # Stop pumps that have run long enough
-            if ml_to_pump_time(ing[i]) < time:
-                pumps[ing[0]].stop()
-
-        sleep(100) # FIXME Is checking every 0.1 enough or too much?
-        time = time.time() - start_time
-
+            sleep(0.1)
+            time = time.time() - start_time
