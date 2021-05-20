26e0d452867ad3a1ca085a3af8f50854c619ef3e
Niko Rintamaki <rr.niko@icoud.com>
2021-05-20 13:42:42 +0300

    Added database queries for history and prevented wrong drinks appearing. Recipes now work.

--- databasequeries.py
+++ databasequeries.py
@@ -12,16 +12,23 @@ class DatabaseQueries:
             SELECT DISTINCT drink FROM drinks
             JOIN recipes ON drinks.id = recipes.drink_id
             JOIN ingredients ON recipes.ingredient_id = ingredients.id
-            JOIN pumps ON ingredients.id = pumps.ingredient_id
-            WHERE pumps.ingredient_id IS NOT NULL
+            LEFT JOIN pumps ON ingredients.id = pumps.ingredient_id
+            WHERE drink_id IN (
+                SELECT drink_id FROM drinks
+                JOIN recipes ON drinks.id = recipes.drink_id
+                JOIN ingredients ON recipes.ingredient_id = ingredients.id
+                LEFT JOIN pumps ON ingredients.id = pumps.ingredient_id
+                GROUP BY 1
+                HAVING bool_and(pumps.id IS NOT NULL)
+            )
             """
 
     AVAILABLE_RECIPE = """
             SELECT pumps.id, quantity FROM drinks
             JOIN recipes ON drinks.id = recipes.drink_id
             JOIN ingredients ON recipes.ingredient_id = ingredients.id
-            LEFT JOIN pumps ON pumps.ingredient_id = ingredients.id
-            WHERE drink=%s AND pumps.ingredient_id IS NOT NULL
+            JOIN pumps ON pumps.ingredient_id = ingredients.id
+            WHERE drink=%s
             """
 
     RECIPE = """
@@ -78,18 +85,40 @@ class DatabaseQueries:
             WHERE name=%s
             """
 
+    USER_HISTORY = """
+            SELECT drinks.drink, timestamp FROM orders
+            JOIN users on users.id = orders.user_id
+            JOIN drinks on drinks.id = orders.drink_id
+            WHERE users.name=%s
+            """
+
     DELETE_USER = """
             DELETE FROM users
             WHERE id=%s
             """
 
     GET_PUMPS_INGREDIENTS = """
-        SELECT pumps.id, ingredients.id, ingredients.ingredient FROM pumps
-        LEFT JOIN ingredients on pumps.ingredient_id = ingredients.id
-    """
+            SELECT pumps.id, ingredients.id, ingredients.ingredient FROM pumps
+            LEFT JOIN ingredients on pumps.ingredient_id = ingredients.id
+            """
 
     SET_PUMP_INGREDIENTS = """
-        INSERT INTO pumps (id, ingredient_id)
-        VALUES (%s, (NULLIF(%s,'None')::integer))
-        ON CONFLICT (id) DO UPDATE SET id = EXCLUDED.id, ingredient_id = EXCLUDED.ingredient_id
-    """
+            INSERT INTO pumps (id, ingredient_id)
+            VALUES (%s, (NULLIF(%s,'None')::integer))
+            ON CONFLICT (id) DO UPDATE SET id = EXCLUDED.id, ingredient_id = EXCLUDED.ingredient_id
+            """
+
+    ORDER = """
+            INSERT INTO orders (user_id, drink_id, timestamp)
+            VALUES (%s, %s, %s)
+            """
+
+    GET_DRINK_ID = """
+            SELECT drinks.id FROM drinks
+            WHERE drink=%s
+            """
+
+    GET_USER_INFO = """
+            SELECT id, name, img, admin FROM users
+            WHERE name=%s
+            """
