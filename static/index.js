ec057193c35b943d80293ea957b393af64265e47
Erkka Mäkinen <erkka.makinen@hillcode.fi>
2021-08-03 17:15:58 +0300

    Moar changes

--- static/index.js
+++ static/index.js
@@ -2,7 +2,7 @@ function login() {
   var loginContent = document.getElementById('login-content');
   var originalImage = loginContent.getAttribute('src');
 
-  loginContent.setAttribute('src', './left-arrow.png');
+  loginContent.setAttribute('src', loginSpinner);
 
   // Return original icon
   setTimeout(function () {
